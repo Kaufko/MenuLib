0d81569a38f43ec4251592172776abb050dc30d7
Kaufko <99084056+Kaufko@users.noreply.github.com>
2024-05-31 13:55:17 +0200

    Fixed all errors.
    Fixed all errors with custom controls and history

--- MenuLib/Menu.cs
+++ MenuLib/Menu.cs
@@ -1,23 +1,28 @@
-namespace MenuLib
+namespace MenuLibrary
 {
-    public static class MenuK
+    public static class MenuLib
     {
         #region 
         static public List<Menu> activeMenu = new List<Menu>();
         static public int selectionIndex = 0;
         static public int currentHistoryIndex = 10;
-        static public List<KeyValuePair<int, Menu>> menuHistory = new List<KeyValuePair<int, Menu>>();
+        static public List<KeyValuePair<int, List<Menu>>> menuHistory = new List<KeyValuePair<int, List<Menu>>>();
         #endregion
 
         #region Keys
-        static public ConsoleKeyInfo keyPress = ConsoleKey.Enter;
-        static public ConsoleKeyInfo keyBack = ConsoleKey.LeftArrow;
-        static public ConsoleKeyInfo keyForward = ConoleKey.RightArrow;
-        static public ConsoleKeyInfo keyQuit = ConsoleKey.Escape;
-        static public ConsoleKeyInfo keyMainMenu = ConsoleKey.Space;
-        static public ConsoleKeyInfo keyUp = ConsoleKey.UpArrow;
-        static public ConsoleKeyInfo keyDown = ConsolKey.DownArrow;
+        static public ConsoleKeyInfo keyPress = KeyChange(ConsoleKey.Enter);
+        static public ConsoleKeyInfo keyBack = KeyChange(ConsoleKey.LeftArrow);
+        static public ConsoleKeyInfo keyForward = KeyChange(ConsoleKey.RightArrow);
+        static public ConsoleKeyInfo keyQuit = KeyChange(ConsoleKey.Escape);
+        static public ConsoleKeyInfo keyMainMenu = KeyChange(ConsoleKey.Spacebar);
+        static public ConsoleKeyInfo keyUp = KeyChange(ConsoleKey.UpArrow);
+        static public ConsoleKeyInfo keyDown = KeyChange(ConsoleKey.DownArrow);
         #endregion
+
+        public static ConsoleKeyInfo KeyChange(ConsoleKey key, bool shift = false, bool alt = false, bool ctrl = false)
+        {
+            return new ConsoleKeyInfo((char)key, key, shift, alt, ctrl);
+        }
 
         public static void Start(List<Menu> mainMenu)
         {
@@ -28,34 +33,34 @@ namespace MenuLib
                 Write(activeMenu, selectionIndex);
                 keyRead = default;
                 keyRead = Console.ReadKey();
-                if (keyRead.Key == keyPress)
+                if (keyRead == keyPress)
                 {
                     Console.Clear();
                     activeMenu[selectionIndex].Action.Invoke();
                 }
-                else if (keyRead.Key == keyUp && selectionIndex - 1 >= 0)
+                else if (keyRead == keyUp && selectionIndex - 1 >= 0)
                 {
                     selectionIndex--;
                 }
-                else if (keyRead.Key == keyDown && selectionIndex + 1 < activeMenu.Count)
+                else if (keyRead == keyDown && selectionIndex + 1 < activeMenu.Count)
                 {
                     selectionIndex++;
                 }
-                else if (keyRead.Key == keyMainMenu)
+                else if (keyRead == keyMainMenu)
                 {
                     selectionIndex = 0;
                     activeMenu = mainMenu;
                 }
-                else if(keyRead.Key == keyBack)
-                { 
-                    GoBack()
-                }
-                else if(keyRead.Key == keyForward)
+                else if(keyRead == keyBack)
                 {
-                    GoForwards()
+                    GoBack();
+                }
+                else if(keyRead == keyForward)
+                {
+                    GoForwards();
                 }
             }
-            while (keyRead.Key != keyQuit);
+            while (keyRead != keyQuit);
         }
 
         public static void Write(List<Menu> activeMenu, int selectionIndex)
@@ -78,8 +83,8 @@ namespace MenuLib
 
         public static void SelectSubMenu(List<Menu> menu)
         {
-            menuHistory.Remove(10);
-            menuHistory.Insert(0, KeyValuePair<selectionIndex, activeMenu>);
+            menuHistory.RemoveAt(10);
+            menuHistory.Insert(0, new KeyValuePair<int, List<Menu>>(selectionIndex, activeMenu));
             currentHistoryIndex = 10;
             selectionIndex = 0;
             activeMenu = menu;
